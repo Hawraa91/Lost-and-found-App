52690bc72e431f9f2bc50c716048c8c9a430aa2e
Hawraa91 <hawroons03@gmail.com>
2024-02-10 17:56:34 +0300

    Start Project

--- lib/main.dart
+++ lib/main.dart
@@ -10,63 +10,87 @@ class MyApp extends StatelessWidget {
     const String appTitle = 'FindMyThing';
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: appTitle,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              // Text section
-              const TextSection(
-                desc:
-                    'Find whats lost, reunite whats found. Together, lets make lost things found',
-              ),
-              const ImageSection(
-                image: 'images/pic.png',
-              ),
-
-              // Signup button
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Container(
-                  width: 200, // match parent width
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(
-                        96, 172, 182, 1.0), // change color as needed
-                    borderRadius:
-                        BorderRadius.circular(8.0), // apply border radius
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      // Handle signup button press
-                    },
-                    child: const Text(
-                      'Sign Up',
-                      style: TextStyle(color: Colors.white),
+        // appBar: AppBar(
+        //   title: const Text(appTitle),
+        // ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                // Text section
+                const TitleSection(
+                  desc: 'FindMyThing',
+                ),
+                const TextSection(
+                  desc:
+                  'Find what\'s lost, reunite what\'s found. Together, let\'s make lost things found',
+                ),
+                const ImageSection(
+                  image: 'images/pic.png',
+                ),
+                // Signup button
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Container(
+                    width: 200, // match parent width
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(
+                          96, 172, 182, 1.0), // change color as needed
+                      borderRadius:
+                      BorderRadius.circular(8.0), // apply border radius
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        // Handle signup button press
+                      },
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
-              ),
-
-              TextButton(
-                onPressed: () {
-                  // Handle link tap
-                },
-                child: const Text(
-                  'got an account? Log in',
+                //the login click
+                TextButton(
+                  onPressed: () {
+                    // Handle link tap
+                  },
+                  child: const Text(
+                    'got an account? Log in',
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+//Title field
+class TitleSection extends StatelessWidget {
+  const TitleSection({Key? key, required this.desc}) : super(key: key);
 
-// Title field
+  final String desc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        desc,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 28, // Adjust the font size as needed
+          fontWeight: FontWeight.bold, // Make the text bold
+        ),
+        softWrap: true,
+      ),
+    );
+  }
+}
+// Text field
 class TextSection extends StatelessWidget {
   const TextSection({Key? key, required this.desc}) : super(key: key);
 
