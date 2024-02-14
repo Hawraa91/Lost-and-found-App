4c7329d0724d376d3bb6803e5467f6545d8ded74
Hawraa91 <hawroons03@gmail.com>
2024-02-14 11:28:56 +0300

    login + firebase

--- lib/routes/login_page.dart
+++ lib/routes/login_page.dart
@@ -1,77 +1,101 @@
 import 'package:flutter/material.dart';
 import '../ImageSection.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(const Login());
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  late Future<FirebaseApp> _initializeFirebase;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeFirebase = _initializeFirebaseApp();
+  }
+
+  Future<FirebaseApp> _initializeFirebaseApp() async {
+    return await Firebase.initializeApp();
+  }
+
+  //Adding the Input Fields method
+  Widget input(String label, IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: label,
+          border: const OutlineInputBorder(),
+          prefixIcon: Icon(icon),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    //Adding the Text Fields method
-    Widget input(String label, IconData icon) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32),
-        child: TextField(
-          decoration: InputDecoration(
-            labelText: label,
-            border: const OutlineInputBorder(),
-            prefixIcon: Icon(icon),
-          ),
-        ),
-      );
-    }
-
-return MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                const ImageSection(image: 'images/login.png', width: 200, height: 250),
-                const TitleSection(desc: 'Login'),
-                //const SizedBox(height: 20), // Add some spacing between the image and input fields
-                input('Email', Icons.email),
-                const SizedBox(height: 10), // Add some spacing between input fields
-                input('Password', Icons.lock),
-                //TODO: the forgot password page
-                const SizedBox(height: 10), // Add some spacing between input fields and login button
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: Container(
-                    width: 200, // match parent width
-                    decoration: BoxDecoration(
-                      color: const Color.fromRGBO(
-                          96, 172, 182, 1.0), // change color as needed
-                      borderRadius:
-                      BorderRadius.circular(8.0), // apply border radius
-                    ),
-
-                    child: TextButton(
-                      onPressed: () {
-                        //navigate to home page
-                      },
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white),
+          child: FutureBuilder(
+            future: _initializeFirebase,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else {
+                return SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      const ImageSection(image: 'images/login.png', width: 200, height: 250),
+                      const TitleSection(desc: 'Login'),
+                      //const SizedBox(height: 20), // Add some spacing between the image and input fields
+                      input('Email', Icons.email),
+                      const SizedBox(height: 10), // Add some spacing between input fields
+                      input('Password', Icons.security),
+                      //TODO: the forgot password page
+                      const SizedBox(height: 10), // Add some spacing between input fields and login button
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: Container(
+                          width: 200, // match parent width
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(
+                                96, 172, 182, 1.0), // change color as needed
+                            borderRadius: BorderRadius.circular(8.0), // apply border radius
+                          ),
+                          child: TextButton(
+                            onPressed: () {
+                              //navigate to home page
+                            },
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                      TextButton(
+                        onPressed: () {
+                          //navigate back to signup page
+                          //TODO: change the navigation to go to the signup page
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          'new account? Sign Up',
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-
-                TextButton(
-                  onPressed: () {
-                    //navigate back to signup page
-                    //TODO: change the navigation to go to the signup page
-                    Navigator.pop(context);
-                  },
-                  child: const Text(
-                    'new account? Sign Up',
-                  ),
-                ),
-              ],
-            ),
+                );
+              }
+            },
           ),
         ),
       ),
