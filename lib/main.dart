8e1d6cca187d50bdab72dbb3e24dc06465a85912
Hawraa91 <hawroons03@gmail.com>
2024-02-20 09:47:19 +0300

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
-import 'package:flutter/foundation.dart';
+//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'routes.dart'; // Import routes from route.dart
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if(kIsWeb){
-    await Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyCVM7_S30Mef_3p34LiCqVQ6vULNCuXilU", appId: "1:336882153933:web:3f9f6fdba5931b1b06ac84", messagingSenderId: "336882153933", projectId: "findmything-9663a"));
-  }
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyDtmIZXK524Gtjt8FDdTafoKD9lfmYlGso",
+      appId: "1:336882153933:android:16afdc1d5782301706ac84",
+      messagingSenderId: "336882153933",
+      projectId: "findmything-9663a",
+    ),
+  );
+
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   @override
