156ad23d6e6a2940f2ca47e774b974fbdd37a28a
Hawraa91 <hawroons03@gmail.com>
2024-02-20 16:13:00 +0300

    Login Page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'routes.dart'; // Import routes from route.dart
 import 'package:firebase_core/firebase_core.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,14 +18,33 @@ void main() async {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: routes, // Use routes defined in route.dart
+    return FutureBuilder(
+      // Check the login status and build the app accordingly
+      future: FirebaseAuth.instance.authStateChanges().first,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Return a loading indicator while waiting for authentication state
+          return CircularProgressIndicator();
+        }
+        if (snapshot.hasData) {
+          // User is logged in
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: '/home', // Assuming '/home' is your home route
+            routes: routes, // Use routes defined in route.dart
+          );
+        } else {
+          // User is not logged in
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: '/', // Assuming '/login' is your login route
+            routes: routes, // Use routes defined in route.dart
+          );
+        }
+      },
     );
   }
 }
