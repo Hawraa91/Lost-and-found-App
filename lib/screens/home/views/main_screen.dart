c60ac9287537c68a0b2a11bf3b333947a9ae90da
Hawraa91 <132151739+Hawraa91@users.noreply.github.com>
2024-03-13 16:56:15 +0300

    matching function

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -2,29 +2,48 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import "package:intl/intl.dart";
+import 'package:intl/intl.dart';
+
+import '../../post/model/search.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<String?>(
+      future: getCurrentUserID(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text('Error: ${snapshot.error}'),
+            ),
+          );
+        } else {
+          final currentUserID = snapshot.data;
+          if (currentUserID == null) {
+            return const Scaffold(
+              body: Center(
+                child: Text('User is not logged in.'),
+              ),
+            );
+          } else {
+            searchLostItemsForCurrentUser(currentUserID);
+            return buildMainScreen(currentUserID);
+          }
+        }
+      },
+    );
+  }
 
-    //a function to retrieve the current user's ID
-    Future<String?> getCurrentUserID() async {
-      FirebaseAuth auth = FirebaseAuth.instance;
-      User? user = auth.currentUser;
-      if (user != null) {
-        return user.uid;
-      }
-      else {
-        return null; // User is not logged in
-      }
-    }
-
-    //saving the current user's ID
-    String currentUser = getCurrentUserID() as String;
-
+  Widget buildMainScreen(String currentUserID) {
     return SafeArea(
       child: SingleChildScrollView(
         child: Padding(
@@ -57,7 +76,7 @@ class MainScreen extends StatelessWidget {
                       const SizedBox(
                         width: 8,
                       ),
-                      Column(
+                      const Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
@@ -65,7 +84,7 @@ class MainScreen extends StatelessWidget {
                             style: TextStyle(
                               fontSize: 12,
                               fontWeight: FontWeight.w600,
-                              color: Theme.of(context).colorScheme.outline,
+
                             ),
                           ),
                           Text(
@@ -73,7 +92,7 @@ class MainScreen extends StatelessWidget {
                             style: TextStyle(
                               fontSize: 18,
                               fontWeight: FontWeight.bold,
-                              color: Theme.of(context).colorScheme.onBackground,
+
                             ),
                           ),
                         ],
@@ -92,7 +111,7 @@ class MainScreen extends StatelessWidget {
               const SizedBox(height: 30),
               const Row(
                 children: [
-                   Text(
+                  Text(
                     "Categories",
                     style: TextStyle(
                       fontSize: 20,
@@ -182,96 +201,107 @@ class MainScreen extends StatelessWidget {
       ),
     );
   }
-}
 
-//The category icons method
-Widget cardIcon(IconData icon, Color backgroundColor,String type) {
-  return Container(
-    height: 65,
-    width: 100,
-    child: Card(
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.5),
-            child: Container(
-              color: backgroundColor,
-              child: Icon(icon),
+  //The category icons method
+  Widget cardIcon(IconData icon, Color backgroundColor,String type) {
+    return Container(
+      height: 65,
+      width: 100,
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.5),
+              child: Container(
+                color: backgroundColor,
+                child: Icon(icon),
+              ),
             ),
-          ),
-          Text(
-            type, // Add your text here
-            style: const TextStyle(
-              fontSize: 12, // Set the font size as per your requirement
-              fontWeight: FontWeight.bold, // Set the font weight as per your requirement
+            Text(
+              type, // Add your text here
+              style: const TextStyle(
+                fontSize: 12, // Set the font size as per your requirement
+                fontWeight: FontWeight.bold, // Set the font weight as per your requirement
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-Widget containerPost(BuildContext context, String title, String desc, String category, DateTime date) {
-  return Container(
-    width: MediaQuery.of(context).size.width,
-    height: MediaQuery.of(context).size.width / 2,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(30),
-      boxShadow: [
-        BoxShadow(
-          blurRadius: 4,
-          color: Colors.grey.shade300,
-          offset: const Offset(5, 5),
-        )
-      ],
-      // Dark blue
-      color: const Color.fromRGBO(96, 173, 183, 1.0),
-    ),
-    child: Padding(
-      padding: const EdgeInsets.all(20.0), // Adjust padding as needed
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center, // Center items vertically
-        crossAxisAlignment: CrossAxisAlignment.center, // Center items horizontally
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            desc,
-            style: const TextStyle(
-              fontSize: 14,
-              color: Colors.white,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            'Category: $category',
-            style: const TextStyle(
-              fontSize: 14,
-              color: Colors.white,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            'Date: ${DateFormat('yyyy-MM-dd').format(date)}',
-            style: const TextStyle(
-              fontSize: 14,
-              color: Colors.white,
-            ),
-            textAlign: TextAlign.center,
-          ),
+  Widget containerPost(BuildContext context, String title, String desc, String category, DateTime date) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.width / 2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 4,
+            color: Colors.grey.shade300,
+            offset: const Offset(5, 5),
+          )
         ],
+        // Dark blue
+        color: const Color.fromRGBO(96, 173, 183, 1.0),
       ),
-    ),
-  );
+      child: Padding(
+        padding: const EdgeInsets.all(20.0), // Adjust padding as needed
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Center items vertically
+          crossAxisAlignment: CrossAxisAlignment.center, // Center items horizontally
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              desc,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Category: $category',
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Date: ${DateFormat('yyyy-MM-dd').format(date)}',
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //a function to retrieve the current user's ID
+  Future<String?> getCurrentUserID() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+    if (user != null) {
+      return user.uid;
+    } else {
+      return null; // User is not logged in
+    }
+  }
 }
