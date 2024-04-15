06b383be8ef4c09f0e50783cc492dc6fc44d8efd
Hawraa91 <132151739+Hawraa91@users.noreply.github.com>
2024-04-15 19:38:38 +0300

    pop up card

--- lib/screens/post/View/SearchResults.dart
+++ lib/screens/post/View/SearchResults.dart
@@ -1,23 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../../components/bottomNavBar.dart';
-class SearchResultsPage extends StatelessWidget {
+import '../../../components/customContainer.dart';
+
+class SearchResultsPage extends StatefulWidget {
   final List<String> matchedTitles;
 
   const SearchResultsPage({Key? key, required this.matchedTitles})
       : super(key: key);
 
   @override
+  _SearchResultsPageState createState() => _SearchResultsPageState();
+}
+
+class _SearchResultsPageState extends State<SearchResultsPage> {
+  List<DocumentSnapshot<Map<String, dynamic>>> _documents = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    //fetching the data of each document ID
+    for (String documentId in widget.matchedTitles) {
+      try {
+        DocumentSnapshot<Map<String, dynamic>> snapshot =
+        await FirebaseFirestore.instance
+            .collection('found')
+            .doc(documentId)
+            .get();
+        if (snapshot.exists) {
+          setState(() {
+            _documents.add(snapshot);
+          });
+        }
+      } catch (error) {
+        print('Error fetching document: $error');
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Use the matchedTitles to display search results
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search Results'),
+        title: const Text('Search Results'),
       ),
       body: ListView.builder(
-        itemCount: matchedTitles.length,
+        itemCount: _documents.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(matchedTitles[index]),
+          // Extract data from document
+          Map<String, dynamic>? data = _documents[index].data();
+          // Display data in ListTile
+          return SingleChildScrollView(
+              child: Column(
+              children: _documents.map((doc) {
+            final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+            final String title = data['itemTitle'] ?? '';
+            final String description = data['description'] ?? '';
+            final String category = data['category'] ?? '';
+            final String dateStr = data['itemLostDate'] ?? ''; // Fetch date as string
+            final DateTime date = DateTime.tryParse(dateStr) ?? DateTime.now(); // Convert string to DateTime, fallback to current time if conversion fails
+
+            return Column(
+              children: [
+                const SizedBox(height: 20), // Add space before the container
+                //containerPost(context, title, description, category, date),
+                CustomContainer(context,
+                    title: title,
+                    desc: description,
+                    category: category,
+                    date: date)
+              ],
+            );
+          }).toList(),
+          ),
           );
         },
       ),
