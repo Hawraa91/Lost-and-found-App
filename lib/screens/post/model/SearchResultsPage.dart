307fa239755823117ea75932cc0d4f7a42a553a4
Hawraa91 <132151739+Hawraa91@users.noreply.github.com>
2024-05-11 21:37:21 +0300

    fixed

--- lib/screens/post/model/SearchResultsPage.dart
+++ lib/screens/post/model/SearchResultsPage.dart
@@ -1,9 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import '../../../components/bottomNavBar.dart';
 import '../../../components/customContainer.dart';
-import '../../chat/chat_page.dart';
 
 class SearchItemResultPage extends StatelessWidget {
   final String title;
